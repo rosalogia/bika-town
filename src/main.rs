99b02f1cd3b72809e628d9fbcb129e486237b03b
Ambika Eshwar <ambika@rosalogia.me>
2021-12-16 00:34:00 -0500

    Tested implementation of character animation, scaled up canvas

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ use sdl2::pixels::Color;
 use sdl2::render::Texture;
 mod models;
 mod rendering;
-use rendering::SpriteSheet;
-use rendering::{WINDOW_HEIGHT, WINDOW_WIDTH};
+use rendering::*;
 
 fn main() {
     let sdl_ctx = sdl2::init().unwrap();
@@ -20,20 +19,31 @@ fn main() {
         .unwrap();
 
     let mut canvas = window.into_canvas().build().unwrap();
+    canvas.set_scale(2.0, 2.0).unwrap();
     let texture_creator = canvas.texture_creator();
     let mut textures: Vec<Texture> = vec![];
     let tile_sheet = SpriteSheet::new(16, 16, &mut textures, &texture_creator, "Assets/Tiles.png");
+    let mut warrior_walk = DirectionalAnimation::new(
+        "Assets/Warrior/",
+        (17, 27),
+        (18, 28),
+        "Movement",
+        &texture_creator,
+        &mut textures,
+    )
+    .unwrap();
+
     'running: loop {
         // let ticks = timer.ticks();
         // let seconds = ticks / 100;
         // let sprite = seconds % 4;
         canvas.set_draw_color(Color::RGB(105, 6, 255));
         canvas.clear();
-        tile_sheet.draw_map(
-            &mut canvas,
-            "Assets/map.tmx",
-            &textures[tile_sheet.texture_id],
-        );
+
+        tile_sheet.draw_map(&textures, &mut canvas, "Assets/map.tmx");
+        warrior_walk
+            .right
+            .draw_animated(0, 0, &textures, &mut canvas);
 
         let mut event_pump = sdl_ctx.event_pump().unwrap();
         for event in event_pump.poll_iter() {
@@ -54,6 +64,6 @@ fn main() {
         }
 
         canvas.present();
-        timer.delay(16);
+        ::std::thread::sleep(::std::time::Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
--- src/rendering.rs
+++ src/rendering.rs
@@ -5,17 +5,17 @@ pub static WINDOW_WIDTH: u32 = 1008;
 pub static WINDOW_HEIGHT: u32 = 1008;
 
 pub struct DirectionalAnimation {
-    up: SpriteSheet,
-    down: SpriteSheet,
-    left: SpriteSheet,
-    right: SpriteSheet,
+    pub up: SpriteSheet,
+    pub down: SpriteSheet,
+    pub left: SpriteSheet,
+    pub right: SpriteSheet,
 }
 
 impl DirectionalAnimation {
     pub fn new<'a, T>(
-        sprite_dir: &std::path::Path,
-        sprite_width: u32,
-        sprite_height: u32,
+        sprite_dir: &str,
+        (lr_width, lr_height): (u32, u32),
+        (ud_width, ud_height): (u32, u32),
         sprite_name: &str,
         texture_creator: &'a TextureCreator<T>,
         textures: &mut Vec<Texture<'a>>,
@@ -25,6 +25,7 @@ impl DirectionalAnimation {
         let mut left: Option<SpriteSheet> = None;
         let mut right: Option<SpriteSheet> = None;
 
+        let sprite_dir = std::path::Path::new(sprite_dir);
         for entry in std::fs::read_dir(sprite_dir)? {
             let path = entry.unwrap().path();
             if path.is_dir() {
@@ -41,8 +42,8 @@ impl DirectionalAnimation {
                 match direction {
                     "Up" => {
                         up = Some(SpriteSheet::new(
-                            sprite_width,
-                            sprite_height,
+                            ud_width,
+                            ud_height,
                             textures,
                             texture_creator,
                             &sprite_path,
@@ -50,8 +51,8 @@ impl DirectionalAnimation {
                     }
                     "Down" => {
                         down = Some(SpriteSheet::new(
-                            sprite_width,
-                            sprite_height,
+                            ud_width,
+                            ud_height,
                             textures,
                             texture_creator,
                             &sprite_path,
@@ -59,8 +60,8 @@ impl DirectionalAnimation {
                     }
                     "Left" => {
                         left = Some(SpriteSheet::new(
-                            sprite_width,
-                            sprite_height,
+                            lr_width,
+                            lr_height,
                             textures,
                             texture_creator,
                             &sprite_path,
@@ -68,8 +69,8 @@ impl DirectionalAnimation {
                     }
                     "Right" => {
                         right = Some(SpriteSheet::new(
-                            sprite_width,
-                            sprite_height,
+                            lr_width,
+                            lr_height,
                             textures,
                             texture_creator,
                             &sprite_path,
@@ -106,6 +107,7 @@ pub struct SpriteSheet {
     sprite_height: u32,
     sprites: Vec<sdl2::rect::Rect>,
     pub texture_id: usize,
+    path: String,
     animation_counter: u32,
 }
 
@@ -141,39 +143,51 @@ impl SpriteSheet {
             }
         }
 
+        let path = String::from(path);
+
         Self {
             sprite_width,
             sprite_height,
             sprites,
             texture_id,
+            path,
             animation_counter: 0,
         }
     }
 
-    pub fn draw_to(&self, i: usize, x: i32, y: i32, texture: &Texture, canvas: &mut WindowCanvas) {
+    pub fn draw_to(
+        &self,
+        i: usize,
+        x: i32,
+        y: i32,
+        textures: &Vec<Texture>,
+        canvas: &mut WindowCanvas,
+    ) {
+        let texture = &textures[self.texture_id];
         let sprite = self.sprites[i];
         let dst = sdl2::rect::Rect::new(x, y, self.sprite_width, self.sprite_height);
         canvas.copy(&texture, sprite, dst).unwrap();
     }
 
-    pub fn draw_animated(&mut self, x: i32, y: i32, texture: &Texture, canvas: &mut WindowCanvas) {
+    pub fn draw_animated(
+        &mut self,
+        x: i32,
+        y: i32,
+        textures: &Vec<Texture>,
+        canvas: &mut WindowCanvas,
+    ) {
         self.draw_to(
             self.animation_counter as usize % self.sprites.len(),
             x,
             y,
-            texture,
+            textures,
             canvas,
         );
 
         self.animation_counter += 1;
     }
 
-    pub fn draw_map(
-        &self,
-        canvas: &mut WindowCanvas,
-        tilemap_path: &str,
-        tile_sheet_texture: &Texture,
-    ) {
+    pub fn draw_map(&self, textures: &Vec<Texture>, canvas: &mut WindowCanvas, tilemap_path: &str) {
         let tilemap = tiled::parse_file(std::path::Path::new(tilemap_path)).unwrap();
         let layer_data = match &tilemap.layers[0].tiles {
             tiled::LayerData::Finite(tiles) => tiles,
@@ -191,7 +205,7 @@ impl SpriteSheet {
                     (gid - tileset.first_gid) as usize,
                     (i * 16) as i32,
                     (j * 16) as i32,
-                    tile_sheet_texture,
+                    textures,
                     canvas,
                 );
             }
